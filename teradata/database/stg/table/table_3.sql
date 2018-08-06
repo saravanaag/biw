571d44290245c414f7d75c840f4ecdcefbee31ea
saravanaag <41623419+saravanaag@users.noreply.github.com>
2018-08-06 11:23:01 +0100

    Update table_3.sql

---
+++ teradata/database/stg/table/table_3.sql
@@ -0,0 +1,12 @@
+CREATE SET TABLE DEV3_BIW_STG.table_3 ,NO FALLBACK ,
+     NO BEFORE JOURNAL,
+     NO AFTER JOURNAL,
+     CHECKSUM = DEFAULT,
+     DEFAULT MERGEBLOCKRATIO
+     (
+      emp_no INTEGER,
+      emp_name VARCHAR(100) CHARACTER SET LATIN NOT CASESPECIFIC,
+      source_system_id INTEGER,
+      xyz integer
+     )
+PRIMARY INDEX ( emp_no );
