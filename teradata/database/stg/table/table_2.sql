c397578165818649e130a995445cec71470031a2
saravanaag <41623419+saravanaag@users.noreply.github.com>
2018-08-06 14:33:07 +0100

    Update table_2.sql

--- teradata/database/stg/table/table_2.sql
+++ teradata/database/stg/table/table_2.sql
@@ -7,7 +7,6 @@ CREATE SET TABLE DEV3_BIW_STG.table_2 ,NO FALLBACK ,
       emp_no2 INTEGER,
       emp_name2 VARCHAR(100) CHARACTER SET LATIN NOT CASESPECIFIC,
       source_system_id2 INTEGER,
-      xyz integer
-     
+      xyz int
      )
 PRIMARY INDEX ( emp_no2 );
