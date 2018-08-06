ad870bb30fe191296ecdbe505021dccc38c587e6
saravanaag <41623419+saravanaag@users.noreply.github.com>
2018-08-06 12:04:25 +0100

    Update table_3.sql

--- teradata/database/stg/table/table_3.sql
+++ teradata/database/stg/table/table_3.sql
@@ -7,6 +7,7 @@ CREATE SET TABLE DEV3_BIW_STG.table_3 ,NO FALLBACK ,
       emp_no INTEGER,
       emp_name VARCHAR(100) CHARACTER SET LATIN NOT CASESPECIFIC,
       source_system_id INTEGER,
-      xyz integer
+      xyz integer,
+      pqr integer not null
      )
 PRIMARY INDEX ( emp_no );
