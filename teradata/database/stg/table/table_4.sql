d51a19aa71ddae24c905e5b57d0387f8246c2469
saravanaag <41623419+saravanaag@users.noreply.github.com>
2018-08-07 09:54:43 +0100

    Create table_4.sql

---
+++ teradata/database/stg/table/table_4.sql
@@ -0,0 +1,12 @@
+CREATE SET TABLE DEV3_BIW_STG.table_4 ,NO FALLBACK ,
+     NO BEFORE JOURNAL,
+     NO AFTER JOURNAL,
+     CHECKSUM = DEFAULT,
+     DEFAULT MERGEBLOCKRATIO
+     (
+      emp_no2 INTEGER,
+      emp_name2 VARCHAR(100) CHARACTER SET LATIN NOT CASESPECIFIC,
+      source_system_id2 INTEGER,
+      xyz int
+     )
+PRIMARY INDEX ( emp_no2 );
