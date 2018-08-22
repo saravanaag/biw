f06f0aa57c234fb205824a4251e3f5208c01f477
saravanaag <41623419+saravanaag@users.noreply.github.com>
2018-08-22 12:07:22 +0100

    Update table_1.sql

--- teradata/database/stg/table/table_1.sql
+++ teradata/database/stg/table/table_1.sql
@@ -5,6 +5,7 @@ CREATE SET TABLE DEV3_BIW_STG.table_1 ,NO FALLBACK ,
      DEFAULT MERGEBLOCKRATIO
      (
       emp_no INTEGER,
+          
       emp_name VARCHAR(100) CHARACTER SET LATIN NOT CASESPECIFIC,
       abcd integer,
       source_system_id INTEGER)
